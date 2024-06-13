31442b3649dc2da5b4592be8ed73e3e5ef5d4c6e
zefanyaeliezer <zefanya261123@gmail.com>
2024-06-13 17:40:39 +0700

    crystal

---
+++ PROJECT PSD/Handler/ReportHandler.cs
@@ -0,0 +1,17 @@
+﻿using PROJECT_PSD.Models;
+using PROJECT_PSD.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PROJECT_PSD.Handler
+{
+    public class ReportHandler
+    {
+        public static List<TransactionHeader> GetAllTransactionHeader()
+        {
+            return ReportRepository.GetAllTrHeaders();
+        }
+    }
+}
